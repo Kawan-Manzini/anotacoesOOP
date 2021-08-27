4c64c305f362dc1170ead59889c53d65db49c4d4
Kawan Manzini <kawanmanzini@gmail.com>
2021-08-27 17:06:49 -0300

    commit 7

---
+++ OOP/Conta.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP
+{
+     //Adicionar um modificador, se não a classe ficara vazia e não conseguiremos chama la 
+    public class Conta
+    {
+        //Get significa pode ser acessado
+        //set significa que vc pd adicionar um valor ao Saldo
+        public double Saldo { get; set; }
+
+        public double Limite { get; set; }
+
+        public int Numero { get; set; }
+        //Quando instaciamos um modelo estamos criando um objeto
+    }
+}
---
+++ OOP/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("aprendendo OOP");
+            // Para cria um objeto, Vc precisa criar uma instância da class Conta
+            // new quer dizer q quero criar uma instância 
+            //Criando instância
+            Conta conta1 = new Conta();
+
+            //criando segunda instância
+            Conta conta2 = new Conta();
+
+            //Valores atribuidos a conta 1
+            conta1.Saldo = 1500;
+            conta1.Limite = 500;
+            conta1.Numero = 123;
+
+            // valores atribuidos a conta 2
+            conta2.Saldo = 2000;
+            conta2.Limite = 600;
+            conta2.Numero = 456;
+
+            Console.WriteLine("Olá, Usuário, seu saldo em conta é: " + conta1.Saldo);
+            Console.WriteLine("Limite de saque é: " + conta1.Limite);
+            Console.WriteLine("O numero de conta é: " + conta1.Numero) ;
+
+
+            Console.WriteLine("________________________________________________________________________");
+
+            Console.WriteLine("Olá, Usuário, seu saldo em conta é: " + conta2.Saldo);
+            Console.WriteLine("Limite de saque é: " + conta2.Limite);
+            Console.WriteLine("O numero de conta é: " + conta2.Numero);
+        }
+    }
+}
