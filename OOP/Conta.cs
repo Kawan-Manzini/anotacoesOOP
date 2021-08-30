c98450755e130d851656bfc31e2f1133f59be30e
Kawan Manzini <kawanmanzini@gmail.com>
2021-08-30 12:54:43 -0300

    commit 8

--- OOP/Conta.cs
+++ OOP/Conta.cs
@@ -9,11 +9,42 @@ namespace OOP
     {
         //Get significa pode ser acessado
         //set significa que vc pd adicionar um valor ao Saldo
-        public double Saldo { get; set; }
+        private double Saldo { get;  set; } // privou outra classe de setar um valor dentro de Saldo
 
-        public double Limite { get; set; }
+        public double Limite { get; private set; }// privou set de Limite pois ja criamos um metodo para determinar um Limite 
 
         public int Numero { get; set; }
         //Quando instaciamos um modelo estamos criando um objeto
+
+        // void significa que este metodo não retornará nada 
+        public void Deposita(double valor)// metodo para deposito de valor 
+        {
+            // this é usado para manipular os valores na classe 
+            this.Saldo += valor;
+        }
+
+        public void AdicionarLimite( double valor) // metodo para gerenciar o valor de limite 
+        {
+            this.Limite = valor;
+        }
+
+        public bool Saque(double valor)
+        {
+            double SaldoDisponivel = this.ConsultaSaldoDisponivel();
+
+            if (valor > SaldoDisponivel)
+            {
+                Console.WriteLine("Erro na operação! Saque e Saldo indisponivel");
+                return false;
+            }
+            this.Saldo -= valor;
+            return true;
+        }
+
+        public double ConsultaSaldoDisponivel() // metodo para consulta de saldo disponivel 
+        {
+            //return pois usamos Double e ele precisa de um retorno diferente de void 
+            return this.Saldo + this.Limite;
+        }
     }
 }
--- OOP/Program.cs
+++ OOP/Program.cs
@@ -10,31 +10,27 @@ namespace OOP
             // Para cria um objeto, Vc precisa criar uma instância da class Conta
             // new quer dizer q quero criar uma instância 
             //Criando instância
-            Conta conta1 = new Conta();
+            Conta conta = new Conta();
 
-            //criando segunda instância
-            Conta conta2 = new Conta();
+            conta.AdicionarLimite(1500);
 
-            //Valores atribuidos a conta 1
-            conta1.Saldo = 1500;
-            conta1.Limite = 500;
-            conta1.Numero = 123;
+            conta.Deposita(1500);
+            conta.Deposita(2500);
+            conta.Deposita(4500);
 
-            // valores atribuidos a conta 2
-            conta2.Saldo = 2000;
-            conta2.Limite = 600;
-            conta2.Numero = 456;
+           bool saca = conta.Saque(7000);
 
-            Console.WriteLine("Olá, Usuário, seu saldo em conta é: " + conta1.Saldo);
-            Console.WriteLine("Limite de saque é: " + conta1.Limite);
-            Console.WriteLine("O numero de conta é: " + conta1.Numero) ;
+            if (saca)
+            {
+                //armazenando retorno do objeto em uma variavel 
+                double saldo = conta.ConsultaSaldoDisponivel();
+
+                Console.WriteLine("Seu saldo é: " + saldo);
+                Console.WriteLine("Seu limite é: " + conta.Limite);
+                // para set (setar um valor)  o limite esta bloqueado mas para get (buscar) esta publico 
+            }
 
 
-            Console.WriteLine("________________________________________________________________________");
-
-            Console.WriteLine("Olá, Usuário, seu saldo em conta é: " + conta2.Saldo);
-            Console.WriteLine("Limite de saque é: " + conta2.Limite);
-            Console.WriteLine("O numero de conta é: " + conta2.Numero);
         }
     }
 }
